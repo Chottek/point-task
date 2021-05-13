fdfd16e884456b3b48b113275ac0350f6c36905d
Chottek <49697145+Chottek@users.noreply.github.com>
2021-05-13 20:42:04 +0200

    Initial commit

---
+++ src/main/java/pl/fox/pointtask/PointtaskApplication.java
@@ -0,0 +1,13 @@
+package pl.fox.pointtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PointtaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PointtaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/fox/pointtask/PointtaskApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.fox.pointtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PointtaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
